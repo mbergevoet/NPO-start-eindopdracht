3c59a7ddafef11274b9cf18691bd0e5d42c05a80
mbergevoet <34296504+mbergevoet@users.noreply.github.com>
2019-09-24 15:50:54 +0200

    Add files via upload

---
+++ Eindopdracht v2/scripts/script.js
@@ -0,0 +1,8 @@
+console.log("Gekoppelt");
+
+var menuButton = document.querySelector("#hamburgerknop");
+var ul = document.querySelector("#mobielnavul");
+
+menuButton.addEventListener("click", function(){
+    ul.classList.toggle("active");
+});
